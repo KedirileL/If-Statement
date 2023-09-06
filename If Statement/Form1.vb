3a5e8973469f898a394967fc22312519a048fa3b
user <user@LAPTOP-KAN0NU4I>
2023-09-06 14:00:19 +0200

    Add project files.

---
+++ If Statement/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub btnGreeting_Click(sender As Object, e As EventArgs) Handles btnGreeting.Click
+        Dim Country As String
+
+        Country = txtCountry.Text.ToLower
+
+        If Country = "south africa" Then
+            MsgBox("Thobela")
+
+        ElseIf Country = "france" Then
+            MsgBox("bonjou")
+
+        ElseIf Country = "china" Then
+            MsgBox("hongcheahong")
+
+        ElseIf Country = "india" Then
+            MsgBox("Habibi")
+
+        Else
+            MsgBox("Hello")
+
+        End If
+    End Sub
+End Class
